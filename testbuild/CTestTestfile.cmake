1ea039d3f5aaf13cf8c918a9fc926f79761ddc7d
Benjamin ALBOUY-KISSI <b.albouy.kissi@gmail.com>
2022-05-03 15:32:17 +0200

    Add test coverage target only if gcovr is present

---
+++ testbuild/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Woazim/source/repos/glshaderpp
+# Build directory: C:/Users/Woazim/source/repos/glshaderpp/testbuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("GLShaderPP")
+subdirs("test")
